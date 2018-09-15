add033faff0eaf54a05635f8991a1d23f78ac6cd
João Veiga <joao.veiga@dreamlab.net>
2018-09-15 22:48:17 +0200

    Receives asynchronous error from playMusic

--- main.go
+++ main.go
@@ -100,27 +100,24 @@ func getSoundFile(device DeviceInfo, configs []Config) string {
 	return ""
 }
 
-func playMusic(musicChannel <-chan MusicInfo) error {
-	for {
-		select {
-		case musicInfo := <-musicChannel:
-			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-			defer cancel()
+func playMusic(musicChannel <-chan MusicInfo, errChan chan<- error) {
+	for musicInfo := range musicChannel {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
 
-			musicCmd := exec.CommandContext(ctx, "sh", "-c", musicInfo.Command+" "+musicInfo.SoundFile)
-			var out bytes.Buffer
-			var stderr bytes.Buffer
-			musicCmd.Stdout = &out
-			musicCmd.Stderr = &stderr
-			err := musicCmd.Run()
-			if err != nil {
-				log.Printf(fmt.Sprint(err) + ": " + stderr.String())
-				return err
-			}
-			if ctx.Err() == context.DeadlineExceeded {
-				log.Println("Deadline exceeded", ctx.Err())
-				return err
-			}
+		musicCmd := exec.CommandContext(ctx, "sh", "-c", musicInfo.Command+" "+musicInfo.SoundFile)
+		var out bytes.Buffer
+		var stderr bytes.Buffer
+		musicCmd.Stdout = &out
+		musicCmd.Stderr = &stderr
+		err := musicCmd.Run()
+		if err != nil {
+			log.Printf(fmt.Sprint(err) + ": " + stderr.String())
+			errChan <- err
+		}
+		if ctx.Err() == context.DeadlineExceeded {
+			log.Println("Deadline exceeded", ctx.Err())
+			errChan <- err
 		}
 	}
 }
@@ -135,7 +132,7 @@ func main() {
 	var routerUsername = flag.String("username", "Admin", "The username for your router login")
 	var routerPwd = flag.String("password", "Password", "The password for your router login")
 	var propertyFile = flag.String("property-file", "config.json", "The location of the property file")
-	var delay = flag.Duration("delay", 5, "The delay (in seconds) with which the program will attempt to poll the router for connected devices")
+	var delay = flag.Duration("delay", 5*time.Second, "The delay (in seconds) with which the program will attempt to poll the router for connected devices")
 	flag.Parse()
 
 	config, err := parsePropertyFile(*propertyFile)
@@ -145,7 +142,8 @@ func main() {
 
 	// Initialize the musicInfo channel
 	musicChannel := make(chan MusicInfo)
-	go playMusic(musicChannel)
+	errChan := make(chan error)
+	go playMusic(musicChannel, errChan)
 
 	//Connect to the router
 	dlink := router.DlinkRouter{
@@ -190,6 +188,8 @@ func main() {
 				}
 			}
 			oldDevices = devices
+		case err := <-errChan:
+			log.Fatal(err)
 		}
 	}
 }
